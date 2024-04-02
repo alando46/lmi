1c76b5f4dfdf905b13bb5cb91d8aa8c2fb93b947
alando46 <music2177@gmail.com>
2024-04-01 18:58:02 -0700

    wip

--- include/lmi/api.h
+++ include/lmi/api.h
@@ -24,7 +24,6 @@ namespace lmi {
         return jsoncons::json::parse(file);
     }
 
-    template <typename T>
     class SchemaBase {};
 
 
@@ -38,13 +37,29 @@ namespace lmi {
     // alternatively, create a factory pattern where we take a response and try to validate for each of the schemas we have
     // imagine a tools workflow where we define multiple schema objects when we make a request
     // when making request we ship the schema for each of those objects
-    
-    template <typename T>
-    class LMIBase {
+
+    // is there a way within the schema to search nested schema?
+    // it seems a macro can automatically serialize nested things:
+    //     1. how does this work? ie is there a hook or something that it uses to know it should serialize the (nested) next layer down?
+    //     2. can i make a macro which calls the serialization macro to piggy-back on that functionality and also provides similar functionality for validation schemas?
+    //     3. are there any built-in functionalities within the validation schema to allow for this nested-like functionality?
+
+    //     the goal is to have each class define its own validation schema, and if a class is composed, it only has to define its own validation schema and not any nested
+    //     schemas. the concern would be, if this is done in json directly, it could yield a poor developer experience because then a developer would need to have this schema
+    //     linked in code and in the validation. that might be v0 though...
+
+    // constraint: no nesting until we write a true reflection system.
+    // WE validate the entire chat api json, and then we extract the tool calls out and make them c++ objects
+    // those objects, which are not nested, can validate themselves.
+
+    // new mvp -> write validation schema for chat completion object, extract just the tool calls.
+    // tool call objects are like what is defined beflow, they hold schema and can validate themselves
+
+    class LMIFunction {
     protected:
         jsoncons::json schema_;
         std::string& schemaPath_;
-        lmi::SchemaBase<T> model_;
+        std::unique_ptr<lmi::SchemaBase> model_;
 
     public:
 
@@ -52,13 +67,14 @@ namespace lmi {
             return schemaPath_;
         }
 
-        LMIBase(const std::string& schemaPath, lmi::SchemaBase<T> model) : schemaPath_(schemaPath), model_(model) {
+        LMIFunction(std::string& schemaPath, std::unique_ptr<lmi::SchemaBase> model) : schemaPath_(schemaPath), model_(std::move(model)) {
             validate();
         }
+        virtual ~LMIFunction() = default;
 
         void validate() {
             // load schema
-            jsoncons::json modelSchema = loadSchema(schemaPath_);
+            auto modelSchema = loadSchema(schemaPath_);
             // create validator
             jsoncons::jsonschema::json_validator<json> validator(modelSchema);
             // make string buffer
@@ -70,65 +86,63 @@ namespace lmi {
         }
     };
 
+    // namespace openAI
+    // {
+    //     class Function : public SchemaBase<Function> {
+    //     public:
+    //         std::string name;
+    //         std::string arguments;
+    //     };
 
+    //     class ToolCall : public SchemaBase<ToolCall> {
+    //     public:
+    //         std::string id;
+    //         std::string type;
+    //         Function function;
+    //     };
 
-    namespace openAI
-    {
-        class Function : public SchemaBase<Function> {
-        public:
-            std::string name;
-            std::string arguments;
-        };
+    //     class Message : public SchemaBase<Message> {
+    //     public:
+    //         std::string role;
+    //         jsoncons::json content;
+    //         std::vector<ToolCall> tool_calls;
+    //     };
 
-        class ToolCall : public SchemaBase<ToolCall> {
-        public:
-            std::string id;
-            std::string type;
-            Function function;
-        };
+    //     class Choice : public SchemaBase<Choice> {
+    //     public:
+    //         int index;
+    //         Message message;
+    //         jsoncons::json logprobs;
+    //         std::string finish_reason;
+    //     };
 
-        class Message : public SchemaBase<Message> {
-        public:
-            std::string role;
-            jsoncons::json content;
-            std::vector<ToolCall> tool_calls;
-        };
+    //     class Usage : public SchemaBase<Usage> {
+    //     public:
+    //         int prompt_tokens;
+    //         int completion_tokens;
+    //         int total_tokens;
+    //     };
 
-        class Choice : public SchemaBase<Choice> {
-        public:
-            int index;
-            Message message;
-            jsoncons::json logprobs;
-            std::string finish_reason;
-        };
-
-        class Usage : public SchemaBase<Usage> {
-        public:
-            int prompt_tokens;
-            int completion_tokens;
-            int total_tokens;
-        };
-
-        class ChatCompletionModel : public SchemaBase<ChatCompletionModel> {
-        public:
-            std::string id;
-            std::string object;
-            int64_t created;
-            std::string model;
-            std::string system_fingerprint;
-            std::vector<Choice> choices;
-            Usage usage;
-        };
-    } // namespace openAI
+    //     class ChatCompletionModel : public SchemaBase<ChatCompletionModel> {
+    //     public:
+    //         std::string id;
+    //         std::string object;
+    //         int64_t created;
+    //         std::string model;
+    //         std::string system_fingerprint;
+    //         std::vector<Choice> choices;
+    //         Usage usage;
+    //     };
+    // } // namespace openAI
 
 
 } // namespace lmi
 
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::ChatCompletionModel, id, object, created, model, choices, usage, system_fingerprint)
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Choice, index, message, logprobs, finish_reason)
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Message, role, content, tool_calls)
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::ToolCall, id, type, function)
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Function, name, arguments)
-JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Usage, prompt_tokens, completion_tokens, total_tokens)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::ChatCompletionModel, id, object, created, model, choices, usage, system_fingerprint)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Choice, index, message, logprobs, finish_reason)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Message, role, content, tool_calls)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::ToolCall, id, type, function)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Function, name, arguments)
+// JSONCONS_ALL_MEMBER_TRAITS(lmi::openAI::Usage, prompt_tokens, completion_tokens, total_tokens)
 
 #endif
---
+++ include/lmi/client.h
@@ -0,0 +1,28 @@
+#ifndef LMI_CLIENT_H
+#define LMI_CLIENT_H
+
+#include "lmi/api.h"
+#include <cpr/cpr.h>
+#include <string>
+
+namespace lmi {
+
+    struct LMIMessage {
+        std::string role = "user";
+        std::string content = "";
+    };
+
+    class BackendBase {
+        std::string& endPoint_;
+        std::string& apiKey_;
+        public:
+            BackendBase(std::string& endPoint, std::string& apiKey) : endPoint_(endPoint) apiKey_(apiKey) {}
+
+            std::unique_ptr<lmi::LMIFunction> create(std::string model, LMIMessage messages, const std::vector<std::unique_ptr<LMIFunction>>& responseModel, int retries=3);
+
+    };
+
+}
+
+
+#endif
